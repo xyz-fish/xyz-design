ba1adc36c16afd34399dc5943079e1dea51befae
songyang <610467559@qq.com>
2022-11-29 12:29:31 +0800

    chore: add uno.css & remove lerna

--- packages/base/src/main.tsx
+++ packages/base/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import RouterElement from '@/router'
-
+import 'uno.css'
 import '@unocss/reset/tailwind.css'
 
 import './index.scss'
